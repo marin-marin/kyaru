37d02aed85420cdc5fdb3240ada8701e72466424
marunhao <marunhao@cmiot.chinamobile.com>
2020-05-28 18:30:01 +0800

    update

--- kyaru.js
+++ kyaru.js
@@ -1,4 +1,53 @@
-const app = new Vue({
+var canvas = this.__canvas = new fabric.Canvas('c');
+// create a rect object
+var flipIcon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABWklEQVRYR+3XPUsdURDG8d/9CFa+VIKCjWCVRkU0kEIF0YQUdnZJaWVjYz5BSiGNvTYWgoWIIpgyhWjANJYBbS0tZOBcOCy73vV60cI9MCx7ds48f55TzGzLG6/WG+trAMocWMUfXPfwegbwBVc4zesWAX5iPSWc4TjFeRcww/iMFUxn58fwr/1eBDjBbInY3wwmoO4rgKL4YoqPFTlzuQtVAD/wCZMlRW6xj2/ZtylsYKmGU7UBtjCBhQQTB/MVAL/Sxi6+1hCPlGcBFGvOINyJa4pnQMaquroyphcB5GINQONA40DjQOPA+3NgBB9SG36VZhStNgTz6MvaW96Ot/G9l+34IAn3lxT9jaOsFUdK5G1iHqMdQGq142KNGCQDKoQvOggERDvKYJ4EWMNOEgjRPRzipqa9xbQY6ZbTYNqGGcL/qqE09gfxgLsuRauOjeOy+LH5M2oceAQNmIwhbwQKzQAAAABJRU5ErkJggg==";
+var deleteIcon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABNElEQVRYR+2XsU4CQRCGv2t5AzttKOzsTZSWBzBQKZVPQK3WPAGVUkl4AFo0oaezoIGON6DFTLJnjsnt7Q135ih2utvbmf/LP7mdvYSGIzlB/xp4AO5U7jcwA34sNU8BeAeePCIfwOA/AS6BjRP4UkL37vkK2JaFsDogIgtXvAOkEL71IMfZAYyBdgA7tdrXAr2uy62B53RRO3AIelbPhj/dswMo04KqHhS2oGpxc771KzALhBJ8AF1gGEo2vh8Bc53jA3gFXowCoe1vgNQ9ighgdUCGzMR5qFskFks8AjK0dNTSAjlmZQhJyFDKHst561mICBAdiA5EB2pxoPGTMDTxit6bHOgBn1XUcnL7wLTsOG4BS+CmJogVcAvsywLIvgt3f9c/oVYm+WmVy+4uL7HxO+EvDoFqIaO0EOIAAAAASUVORK5CYII=";
+
+var flipImg = document.createElement('img');
+flipImg.src = flipIcon;
+var delImg = document.createElement('img');
+delImg.src = deleteIcon;
+
+fabric.Object.prototype.controls.flipY = new fabric.Control({
+  position: { x: -0.4, y: -0.6 },
+  cursorStyle: 'pointer',
+  mouseUpHandler: flipObject,
+  render: renderIcon(flipImg),
+  cornerSize: 24
+});
+
+fabric.Object.prototype.controls.deleteControl = new fabric.Control({
+  position: { x: 0.4, y: -0.6 },
+  cursorStyle: 'pointer',
+  mouseUpHandler: deleteObject,
+  render: renderIcon(delImg),
+  cornerSize: 24
+});
+
+function deleteObject(eventData, target) {
+  var canvas = target.canvas;
+    canvas.remove(target)
+    canvas.requestRenderAll();
+}
+
+function flipObject(eventData, target) {
+  var canvas = target.canvas;
+    target.flipX = !target.flipX
+    canvas.requestRenderAll();
+}
+
+function renderIcon (icon) {
+  return function renderIcon(ctx, left, top, styleOverride, fabricObject) {
+    var size = this.cornerSize;
+    ctx.save();
+    ctx.translate(left, top);
+    ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
+    ctx.drawImage(icon, -size/2, -size/2, size, size);
+    ctx.restore();
+  }
+}
+
+new Vue({
   el: '#kyaru',
   data () {
     return {
